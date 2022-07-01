f7ae6ef2f5b421a23eac61c2f80b3480a197ba3a
jankovicdev <stefanjankovic.dev@gmail.com>
2022-07-02 00:16:30 +0200

    Initial commit

---
+++ src/test/java/demo/tripadvisorapp/TripAdvisorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.tripadvisorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripAdvisorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
