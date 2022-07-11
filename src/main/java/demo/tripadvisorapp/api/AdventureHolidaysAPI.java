1898a2789a73f1e4574fc9daa7ca6fbda9cc40d5
jankovicdev <stefanjankovic.dev@gmail.com>
2022-07-11 20:53:10 +0200

    Created full user story for one choice.

--- src/main/java/demo/tripadvisorapp/api/AdventureHolidaysAPI.java
+++ src/main/java/demo/tripadvisorapp/api/AdventureHolidaysAPI.java
@@ -3,13 +3,15 @@ package demo.tripadvisorapp.api;
 import demo.tripadvisorapp.models.AdventureHolidays;
 import demo.tripadvisorapp.services.AdventureHolidaysService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/adventureHolidays")
 public class AdventureHolidaysAPI {
 
@@ -17,12 +19,14 @@ public class AdventureHolidaysAPI {
     private AdventureHolidaysService adventureHolidaysService;
 
     @GetMapping("/getRandomSummerCamps")
-    public List<AdventureHolidays> getRandomSummerCamps() {
-        return adventureHolidaysService.getRandomSummerCamps();
+    public String getRandomSummerCamps(Model model) {
+        model.addAttribute("randomSummerCamps", adventureHolidaysService.getRandomSummerCamps());
+        return "randomSummerCamps";
     }
 
     @GetMapping("/getRandomTrekking")
-    public List<AdventureHolidays> getRandomTrekking() {
-        return adventureHolidaysService.getRandomTrekking();
+    public String getRandomTrekking(Model model) {
+        model.addAttribute("randomTrekking", adventureHolidaysService.getRandomSummerCamps());
+        return "adventureholidays";
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,33 +1,33 @@
 /*Inserting main variables*/
 
-let adventureHolidays = document.getElementById("adventureHolidays");
-let backpacking = document.getElementById("backpacking");
-let cruiseHolidays = document.getElementById("cruiseHolidays");
-let eventTravel = document.getElementById("eventTravel");
-let packageHoliday = document.getElementById("packageHoliday");
-let safari = document.getElementById("safari");
-let skiingAndSnowboarding = document.getElementById("skiingAndSnowboarding");
-let volunteering = document.getElementById("volunteering");
+const adventureHolidays = document.getElementById("adventureHolidays");
+const backpacking = document.getElementById("backpacking");
+const cruiseHolidays = document.getElementById("cruiseHolidays");
+const eventTravel = document.getElementById("eventTravel");
+const packageHoliday = document.getElementById("packageHoliday");
+const safari = document.getElementById("safari");
+const skiingAndSnowboarding = document.getElementById("skiingAndSnowboarding");
+const volunteering = document.getElementById("volunteering");
 /*End of inserting main variables*/
 
 /*sub variables*/
 
-let ahSummerCamps = document.getElementById("aH-summerCamps");
-let ahTrekking = document.getElementById("aH-trekking");
-let bPLongDistanceHiking = document.getElementById("bP-longDistanceHiking");
-let bPThruHiking = document.getElementById("bP-thruHiking");
-let cOceanCruising = document.getElementById("c-oceanCruising");
-let cRiverCruising = document.getElementById("c-riverCruising");
-let eRecreationsEvent = document.getElementById("e-recreationsEvent");
-let eSportsEvent = document.getElementById("e-sportsEvents");
-let phEscortedTours = document.getElementById("pH-escortedTours");
-let phIndependentTours = document.getElementById("pH-independentTours");
-let sPhotographicSafari = document.getElementById("s-photographicSafari");
-let sCyclingSafari = document.getElementById("s-cyclingSafari");
-let sAsBackcountrySkiing = document.getElementById("sAs-backcountrySkiing");
-let sAsDownhillSkiing = document.getElementById("sAs-downhillSkiing");
-let vChildcare = document.getElementById("v-childcare");
-let vConservationAndEnvironment = document.getElementById("v-conservationAndEnvironment");
+const ahSummerCamps = document.getElementById("aH-summerCamps");
+const ahTrekking = document.getElementById("aH-trekking");
+const bPLongDistanceHiking = document.getElementById("bP-longDistanceHiking");
+const bPThruHiking = document.getElementById("bP-thruHiking");
+const cOceanCruising = document.getElementById("c-oceanCruising");
+const cRiverCruising = document.getElementById("c-riverCruising");
+const eRecreationsEvent = document.getElementById("e-recreationsEvent");
+const eSportsEvent = document.getElementById("e-sportsEvents");
+const phEscortedTours = document.getElementById("pH-escortedTours");
+const phIndependentTours = document.getElementById("pH-independentTours");
+const sPhotographicSafari = document.getElementById("s-photographicSafari");
+const sCyclingSafari = document.getElementById("s-cyclingSafari");
+const sAsBackcountrySkiing = document.getElementById("sAs-backcountrySkiing");
+const sAsDownhillSkiing = document.getElementById("sAs-downhillSkiing");
+const vChildcare = document.getElementById("v-childcare");
+const vConservationAndEnvironment = document.getElementById("v-conservationAndEnvironment");
 
 /*end of subs variables*/
 
@@ -37,10 +37,9 @@ window.onload = function () {
 };
 
 const allDivs = ["adventureHolidays", "backpacking", "cruiseHolidays", "eventTravel", "packageHoliday", "safari", "skiingAndSnowboarding", "volunteering", "aH-summerCamps", "aH-trekking", "bP-longDistanceHiking", "bP-thruHiking", "c-oceanCruising", "c-riverCruising", "e-recreationsEvent", "e-sportsEvents", "pH-escortedTours", "pH-independentTours", "s-photographicSafari", "s-cyclingSafari", "sAs-backcountrySkiing", "v-conservationAndEnvironment", "sAs-downhillSkiing", "v-childcare"]
-    .map(id => document.getElementById(id)); // gives you an array of those elements
-
+    .map(id => document.getElementById(id));
 const subDivs = ["aH-summerCamps", "aH-trekking", "bP-longDistanceHiking", "bP-thruHiking", "c-oceanCruising", "c-riverCruising", "e-recreationsEvent", "e-sportsEvents", "pH-escortedTours", "pH-independentTours", "s-photographicSafari", "s-cyclingSafari", "sAs-backcountrySkiing", "v-conservationAndEnvironment", "sAs-downhillSkiing", "v-childcare"]
-    .map(id => document.getElementById(id)); // gives you an array of those elements
+    .map(id => document.getElementById(id));
 
 function hideAllSubDivs(id) {
     for (const div of subDivs) div.hidden = !(div.id === id);
@@ -53,35 +52,75 @@ function hideAllDivsExcept(id, ids) {
 function choice() {
     adventureHolidays.addEventListener("click", () => {
         hideAllDivsExcept('aH-summerCamps', 'aH-trekking');
-        console.log("adventure holidays");
     })
     backpacking.addEventListener("click", () => {
         hideAllDivsExcept('bP-longDistanceHiking', 'bP-thruHiking');
-        console.log("backpacking");
     })
     cruiseHolidays.addEventListener("click", () => {
         hideAllDivsExcept('c-oceanCruising', 'c-riverCruising');
-        console.log("cruise");
     })
     eventTravel.addEventListener("click", () => {
         hideAllDivsExcept('e-recreationsEvent', 'e-sportsEvents');
-        console.log("event");
     })
     packageHoliday.addEventListener("click", () => {
         hideAllDivsExcept('pH-escortedTours', 'pH-independentTours');
-        console.log("package holiday");
     })
     safari.addEventListener("click", () => {
         hideAllDivsExcept('s-photographicSafari', 's-cyclingSafari');
-        console.log("safari");
     })
     skiingAndSnowboarding.addEventListener("click", () => {
         hideAllDivsExcept('sAs-downhillSkiing', 'sAs-backcountrySkiing');
-        console.log("skiing and snowboarding");
     })
     volunteering.addEventListener("click", () => {
         hideAllDivsExcept('v-childcare', 'v-conservationAndEnvironment');
-        console.log("volunteering");
+    })
+    ahSummerCamps.addEventListener("click", () => {
+        location.href = "/adventureHolidays/getRandomSummerCamps";
+    })
+    ahTrekking.addEventListener("click", () => {
+        location.href = "/adventureHolidays/getRandomTrekking";
+    })
+    bPLongDistanceHiking.addEventListener("click", () => {
+        location.href = "/backpacking/getRandomLongDistanceHiking";
+    })
+    bPThruHiking.addEventListener("click", () => {
+        location.href = "/backpacking/getRandomThruHiking";
+    })
+    cOceanCruising.addEventListener("click", () => {
+        location.href = "/cruiseHolidays/getRandomOceanCruising";
+    })
+    cRiverCruising.addEventListener("click", () => {
+        location.href = "/cruiseHolidays/getRandomRiverCruising";
+    })
+    eRecreationsEvent.addEventListener("click", () => {
+        location.href = "/eventTravel/getRandomRecreationsEvents";
+    })
+    eSportsEvent.addEventListener("click", () => {
+        location.href = "/eventTravel/getRandomSportsEvents";
+    })
+    phIndependentTours.addEventListener("click", () => {
+        location.href = "/packageHoliday/getRandomIndependentTours";
+    })
+    phEscortedTours.addEventListener("click", () => {
+        location.href = "/packageHoliday/getRandomEscortedTours";
+    })
+    sPhotographicSafari.addEventListener("click", () => {
+        location.href = "/safari/getRandomPhotographicSafari";
+    })
+    sCyclingSafari.addEventListener("click", () => {
+        location.href = "/safari/getRandomCyclingSafari";
+    })
+    sAsBackcountrySkiing.addEventListener("click", () => {
+        location.href = "/skiingAndSnowboarding/getRandomBackcountrySkiing";
+    })
+    sAsDownhillSkiing.addEventListener("click", () => {
+        location.href = "/skiingAndSnowboarding/getRandomDownhillSkiing";
+    })
+    vChildcare.addEventListener("click", () => {
+        location.href = "/volunteering/getRandomChildcare";
+    })
+    vConservationAndEnvironment.addEventListener("click", () => {
+        location.href = "/volunteering/getRandomConservationAndEnvironment";
     })
 }
 
