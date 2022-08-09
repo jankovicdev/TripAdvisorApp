bb2141b1a156fa9b540063e74404727d95049167
jankovicdev <stefanjankovic.dev@gmail.com>
2022-08-09 19:00:21 +0200

    make it better

--- src/main/java/demo/tripadvisorapp/TripAdvisorAppApplication.java
+++ src/main/java/demo/tripadvisorapp/TripAdvisorAppApplication.java
@@ -3,7 +3,7 @@ package demo.tripadvisorapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TripAdvisorAppApplication {
 
     public static void main(String[] args) {
