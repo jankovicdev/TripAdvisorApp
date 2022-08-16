eec647050c6611ca3370612242b18234cd92c24e
jankovicdev <stefanjankovic.dev@gmail.com>
2022-08-16 02:37:48 +0200

    small refactoring for heroku

--- src/main/java/demo/tripadvisorapp/api/AdventureHolidaysAPI.java
+++ src/main/java/demo/tripadvisorapp/api/AdventureHolidaysAPI.java
@@ -4,9 +4,7 @@ import demo.tripadvisorapp.services.AdventureHolidaysService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
--- src/main/java/demo/tripadvisorapp/repository/BackpackingRepository.java
+++ src/main/java/demo/tripadvisorapp/repository/BackpackingRepository.java
@@ -1,8 +1,6 @@
 package demo.tripadvisorapp.repository;
 
 import demo.tripadvisorapp.models.Backpacking;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BackpackingRepository extends MongoRepository<Backpacking, String> {
