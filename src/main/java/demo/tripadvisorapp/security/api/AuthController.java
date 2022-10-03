c746302939258e2783d64a931832a8080a53d30a
jankovicdev <stefanjankovic.dev@gmail.com>
2022-10-03 16:36:50 +0200

    Registration

--- src/main/java/demo/tripadvisorapp/security/api/AuthController.java
+++ src/main/java/demo/tripadvisorapp/security/api/AuthController.java
@@ -17,15 +17,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.File;
 
-@RestController
+@Controller
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
@@ -43,7 +43,7 @@ public class AuthController {
 
     @PostMapping("/login")
     @Transactional
-    public ResponseEntity<?> login(@Valid @RequestBody LoginDTO dto) {
+    public ResponseEntity<?> login(@Valid @ModelAttribute("login") LoginDTO dto, Model model) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         User user = (User) authentication.getPrincipal();
@@ -52,26 +52,48 @@ public class AuthController {
         refreshToken.setOwner(user);
         refreshTokenRepository.save(refreshToken);
 
+        model.addAttribute("login", dto);
+
         String accessToken = jwtHelper.generateAccessToken(user);
         String refreshTokenString = jwtHelper.generateRefreshToken(user, refreshToken);
 
         return ResponseEntity.ok(new TokenDTO(user.getId(), accessToken, refreshTokenString));
+
     }
 
     @PostMapping("signup")
     @Transactional
-    public ResponseEntity<?> signup(@Valid @RequestBody SignupDTO dto) {
-        User user = new User(dto.getUsername(), dto.getEmail(), passwordEncoder.encode(dto.getPassword()));
+    public ResponseEntity<?> signup(@Valid @ModelAttribute("user") SignupDTO signupDTO, Model model) {
+        User user = new User(signupDTO.getUsername(), signupDTO.getEmail(), passwordEncoder.encode(signupDTO.getPassword()));
+        model.addAttribute("user", signupDTO);
         userRepository.save(user);
 
         RefreshToken refreshToken = new RefreshToken();
         refreshToken.setOwner(user);
         refreshTokenRepository.save(refreshToken);
 
+
         String accessToken = jwtHelper.generateAccessToken(user);
         String refreshTokenString = jwtHelper.generateRefreshToken(user, refreshToken);
 
+
         return ResponseEntity.ok(new TokenDTO(user.getId(), accessToken, refreshTokenString));
+    }
+
+    @GetMapping("/registerForm")
+    public String showRegistrationForm(Model model) {
+        // create model object to store form data
+        SignupDTO user = new SignupDTO();
+        model.addAttribute("user", user);
+        return "signup";
+    }
+
+    @GetMapping("/loginForm")
+    public String showLoginForm(Model model) {
+        // create model object to store form data
+        LoginDTO loginDTO = new LoginDTO();
+        model.addAttribute("login", loginDTO);
+        return "login";
     }
 
     @PostMapping("logout")
